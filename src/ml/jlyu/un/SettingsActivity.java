0f1440dc86073e0fa0bd9df1679d308b598d9ee2
吕海涛 <jalenlv@anjuke.com>
2014-07-15 17:12:38 +0800

    在Activity中处理配置更新逻辑

--- src/ml/jlyu/un/SettingsActivity.java
+++ src/ml/jlyu/un/SettingsActivity.java
@@ -2,12 +2,17 @@ package ml.jlyu.un;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends Activity implements
+		OnSharedPreferenceChangeListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
 		if (savedInstanceState == null) {
 			SettingsFragment fragment = new SettingsFragment();
@@ -16,9 +21,19 @@ public class SettingsActivity extends Activity {
 					.add(android.R.id.content, fragment,
 							fragment.getClass().getSimpleName()).commit();
 		}
-		
-		super.onCreate(savedInstanceState);
 
+		PreferenceManager.getDefaultSharedPreferences(this)
+				.registerOnSharedPreferenceChangeListener(this);
 		this.startService(new Intent(this, UNoticeService.class));
+	}
+
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+			String key) {
+
+		if (sharedPreferences.getString("server_uri", null) != null
+				&& sharedPreferences.getString("topic", null) != null) {
+			sendBroadcast(new Intent(UNoticeService.ACTION_RESTART));
+		}
 	}
 }
